f004481c6f992c7d1bde987f982e0dc876b0bb91
James Lin <james97lin@gmail.com>
2018-12-10 19:33:42 -0800

    Added BBSerializer, with a function to write a json blob with bounding box info to a file

---
+++ ScanNetPP_2017/Assets/BBControl/Scripts/BBSerializer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Takes bounding box transform data and writes it to a JSON file.
+ */
+public class BBSerializer : MonoBehaviour {
+
+    public static string PARENT_NAME = "BoundingBoxes";
+
+    [Serializable]
+    public class BBInfo {
+        public Vector3 position;
+        public Vector3 rotation;
+        public Vector3 scale;
+        public string label;
+    }
+
+    [Serializable]
+    public class BBList {
+        public BBInfo[] boundingBoxes;
+    }
+
+    public void Update() {
+        if (Input.GetKeyDown(KeyCode.S)) {
+            string json = BBToJson();
+            WriteJsonToFile(json, "testing");
+        }
+    }
+
+    /**
+     * Takes all bounding boxes under the bounding box parent object serializes them into a JSON blob string.
+     */
+    public string BBToJson() {
+        GameObject bbParent = GameObject.Find(PARENT_NAME);
+        BBInfo[] boundingBoxes = new BBInfo[bbParent.transform.childCount];
+        for (int i = 0; i < boundingBoxes.Length; i++) {
+            Transform childTransform = bbParent.transform.GetChild(i);
+            BBInfo bbInfo = new BBInfo();
+            bbInfo.position = childTransform.position;
+            bbInfo.rotation = childTransform.eulerAngles;
+            bbInfo.scale = childTransform.localScale;
+            bbInfo.label = childTransform.GetComponent<BBState>().label;
+            boundingBoxes[i] = bbInfo;
+        }
+
+        BBList bbList = new BBList();
+        bbList.boundingBoxes = boundingBoxes;
+        return JsonUtility.ToJson(bbList);
+    }
+
+    /**
+     * Writes the contents of a json blob to a file w/ the provided name.
+     */
+    public void WriteJsonToFile(string json, string name) {
+        string filePath = Application.persistentDataPath + "/" + name + ".txt";
+        print(filePath);
+        System.IO.File.WriteAllText(filePath, json);
+    }
+}
